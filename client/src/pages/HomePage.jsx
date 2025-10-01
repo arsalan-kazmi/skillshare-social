1f9b12a3c5825b0969cf8eb6cfb78bf017fecf0c
CodeWithMomin <mominzahoor11@gmail.com>
2025-10-01 10:40:07 +0530

    Added padding/margin to right sidebar and removed horizontal scroll

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// import './App.css'
+import '../App.css'
+const HomePage = () => {
+  return (
+    <>
+    <div className="header">
+      hhh
+     </div>
+     <div className="container">
+      <div className="left-sidebar">Left</div>
+      <div className="feed">Center</div>
+      <div className="right-sidebar">right</div>
+     </div>
+    </>
+  )
+}
+
+export default HomePage
