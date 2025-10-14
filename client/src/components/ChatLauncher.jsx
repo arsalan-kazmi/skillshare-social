e959743bb02863240653cddb0b3b80b6d048abd5
arsalankazmi7 <arsalankazmi7@gmail.com>
2025-10-14 19:42:33 +0530

    changes

--- client/src/components/ChatLauncher.jsx
+++ client/src/components/ChatLauncher.jsx
@@ -1,23 +1,13 @@
 import React from "react";
 
-import '../App.css'
+import "../App.css";
 const ChatLauncher = () => {
   return (
-<<<<<<< HEAD
-    <div>
-      <img src="src\assets\chatting.png" alt="" />
-    </div>
-=======
-   <>
-   <div className="chatdiv">
-    <img
-        src="src\assets\chatting.png"
-        alt=""
-      />
-   </div>
-
-   </>
->>>>>>> 3b50e14de1e663bebe8c4ef187a4d8943720d65a
+    <>
+      <div className="chatdiv">
+        <img src="src\assets\chatting.png" alt="" />
+      </div>
+    </>
   );
 };
 
--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -4,14 +4,7 @@ const Logo = () => {
   return (
     <>
       <div className="logobox">
-<<<<<<< HEAD
         <img src="src\assets\letter-s.png" alt="" />
-=======
-        <img
-          src="src\assets\letter-s.png"
-          alt=""
-        />
->>>>>>> 3b50e14de1e663bebe8c4ef187a4d8943720d65a
       </div>
     </>
   );
